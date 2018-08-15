86244a3aa2284a90ae520d683caf5b70ea5d3d8e
Sree <harsha6raju@yahoo.com>
2018-08-15 10:11:06 -0400

    add bots to bot army

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,57 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(props) {
+    super(props)
+    this.state = {
+      bots: [],
+      botsArmy: [],
+
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots').then(res => res.json()).then(json => this.setInitialState(json))
+  }
+
+  setInitialState = (botsArray) => {
+    this.setState({
+      bots: botsArray
+    })
+  }
+
+  addBotToBotArmy = (newBot) => {
+    const updatedBotsArmy = this.state.botsArmy
+    updatedBotsArmy.push(newBot)
+
+    const updatedBots = this.state.bots
+
+    const found = updatedBots.find((bot) => {
+      return bot.id === newBot.id
+    })
+
+    const index = updatedBots.indexOf(found)
+
+    updatedBots.splice(index, 1)
+
+    this.setState({
+      bots: updatedBots,
+      botsArmy: updatedBotsArmy,
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.botsArmy}/>
+        <BotCollection bots={this.state.bots} addBotToBotArmy={this.addBotToBotArmy}/>
       </div>
     );
   }
