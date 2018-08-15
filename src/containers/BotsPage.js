76855fa78a389e1f97838be943daed60ac31e4f4
DevinPierce <devinmpierce@gmail.com>
2018-08-15 10:10:41 -0400

    checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,47 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    botArmy: []
+  }
+
+  addBotToArmy = (id) => {
+    if (!this.state.botArmy.some(bot => bot.id === id)){
+      const newBotArmy = [...this.state.botArmy]
+      newBotArmy.push(this.state.bots.find(bot => bot.id === id))
+      this.setState({
+        botArmy: newBotArmy
+      })
+    }
+  }
+
+  removeFromBotArmy = (id) => {
+    const newBotArmy = this.state.botArmy.filter(bot => bot.id !== id)
+    this.setState({
+      botArmy: newBotArmy
+    })
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} removeFromBotArmy={this.removeFromBotArmy} />
+        <BotCollection bots={this.state.bots} addBotToArmy={this.addBotToArmy} />
       </div>
     );
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res=>res.json())
+    .then(botArray=>this.setState({
+      bots: botArray
+    }))
   }
 
 }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,26 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+  renderBotCards(){
+    return this.props.botArmy.map(bot => {
+      return (
+        <BotCard key={bot.id} bot={bot} clickCard={this.props.removeFromBotArmy} />
+      )
+    })
+  }
+
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.renderBotCards()}
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
