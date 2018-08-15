f20d97592d92a338276c6948a8d5864dee3adb1d
github username <github email address>
2018-08-15 10:40:21 -0400

    second part done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,11 +4,32 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+  // <BotCollection bots = {this.state.bots} handleClick = {this.handleClick} />
+  // state = {
+  //     id: 0,
+  //     name: '',
+  //     health: 0,
+  //     damage: 0,
+  //     armor: 0,
+  //     bot_class: '',
+  //     catchphrase: ''
+  //   }
+
+
+
+  renderBots = () => {
+    return this.props.bots.map(botObj => {
+      return (
+        <BotCard bot = {botObj} handleClick = {this.props.handleClick} />
+      )
+    })
+  }
+
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+          {this.renderBots()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,62 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+
+  renderMyBots = (props) => {
+    return this.props.chosenBots.map(bot => {
+      return (
+          <div className="ui column">
+            <div
+              className="ui card"
+              key={bot.id}
+              onClick={(event) => {this.props.handleDelete(event,bot.id)}}
+            >
+              <div className="image">
+                <img alt="oh no!" src={bot.avatar_url} />
+              </div>
+              <div className="content">
+                <div className="header">
+                  {bot.name}
+                </div>
+
+                <div className="meta text-wrap">
+                  <small>{bot.catchphrase}</small>
+                </div>
+              </div>
+              <div className="extra content">
+                <span>
+                  <i className="icon heartbeat" />
+                  {bot.health}
+                </span>
+
+                <span>
+                  <i className="icon lightning" />
+                  {bot.damage}
+                </span>
+                <span>
+                  <i className="icon shield" />
+                  {bot.armor}
+                </span>
+              </div>
+            </div>
+          </div>
+      )
+    })
+  }
+
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.renderMyBots()}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
